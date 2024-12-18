15e9db4981d16e8a23812c70fb3fa7625e82d3d0
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-12-18 17:14:15 +0800

    updated the nav

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,6 @@
-import seeMore from "../assets/seemore.png";
-import img1 from "../assets/1.png";
 import img2 from "../assets/2.png";
 import img3 from "../assets/3.png";
 import img4 from "../assets/4.png";
 
-const images = { seeMore, img2, img1,img3, img4 };
+const images = { img2, img3, img4 };
 export default images;
